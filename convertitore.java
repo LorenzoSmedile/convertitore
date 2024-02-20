5c27a64ec42507169de2fd1d70e6e6621390aaae
Lorenzo Smedile <Lorenzo Smedile>
2024-02-20 11:16:13 +0100

    secondo commit

--- convertitore.java
+++ convertitore.java
@@ -6,53 +6,64 @@ import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
-public class convertitore extends Application{
-	
-	Label lConvertire= new Label(" n° da convertire ");
-	Label lBase= new Label(" base ");
-	Label lNumero= new Label(" numero c. ");
-	TextField tConvertire= new TextField();
-	TextField tBase= new TextField();
-	
+public class convertitore extends Application {
 
-	
-	
+	Label lConvertire = new Label(" n° da convertire ");
+	Label lBase = new Label(" base ");
+	Label lNumero = new Label(" numero c. ");
+	Label lNumeroC = new Label();
+	TextField tConvertire = new TextField();
+	TextField tBase = new TextField();
+	Button bCalcola = new Button("converti");
+
 	public void start(Stage finestra) {
-	
-	GridPane griglia= new GridPane();
-	
-	
-	griglia.add(lConvertire, 0, 0);
-	griglia.add(tConvertire, 1, 0);
-	griglia.add(lBase, 0, 1);
-	griglia.add(tBase, 1, 1);
-	griglia.add(lNumero,0,2);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+		GridPane griglia = new GridPane();
+
+		griglia.add(lConvertire, 0, 0);
+		griglia.add(tConvertire, 1, 0);
+		griglia.add(lBase, 0, 1);
+		griglia.add(tBase, 1, 1);
+		griglia.add(lNumero, 0, 2);
+		griglia.add(lNumeroC, 0, 3);
+		griglia.add(bCalcola, 1, 4);
+
 		Scene scena = new Scene(griglia);
 
-		finestra.setTitle("Prima Interfaccia");
+		finestra.setTitle("Convertitore");
 		finestra.setScene(scena);
 		finestra.show();
-	
-	
-	
+
+		bCalcola.setOnAction(e -> calcola());
+
 	}
+
+	public void calcola() {
+
+		int numero;
+		int base;
+		String risposta = "";
+
+		numero = Integer.parseInt(tConvertire.getText());
+		base = Integer.parseInt(tBase.getText());
+
+		int resto = 0;
+		String s = " ";
+		for(int i=0;numero>0;i++) {
+			resto=numero;
+			numero=numero/base;
+			resto=resto-(numero*base);
+			s=resto+s;
+		}
+		lNumeroC.setText(risposta);
+		
+	}
+		
+
 	
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
